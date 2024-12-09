d033d65fc64e2b87d4e81d3b45a5317b63f751cf
shashi755 <shashivardhanreddythatiparthi@gmail.com>
2024-12-09 17:53:05 +0530

    added

---
+++ Main.java
@@ -0,0 +1,7 @@
+// HelloWorld.java
+public class Main {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
